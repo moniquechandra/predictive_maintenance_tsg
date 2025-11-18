43eaba546f77775c6f1de470c20d84fe965c94ab
Vleespet <75667571+Vleespet@users.noreply.github.com>
2025-11-18 14:14:37 +0100

    Add try, except statement to feather files.

--- Drivedata.py
+++ Drivedata.py
@@ -3,10 +3,22 @@ import pandas as pd
 import glob
 
 def concat_csv_files():
-    csv_files = glob.glob("data/SVRM2_Drive_data/*.csv")
-    dfs = [pd.read_csv(path) for path in csv_files]
-    combined = pd.concat(dfs, ignore_index=True)
-    return combined
+    try:
+        existing = pd.read_feather("data/SVRM2_Drive_data/combined.feather")
+        return existing
+    except:
+        csv_files = glob.glob("data/SVRM2_Drive_data/*.csv")
+        dfs = [pd.read_csv(path,skiprows=5) for path in csv_files]
+        combined = pd.concat(dfs, ignore_index=True)
+        combined.to_feather("data/SVRM2_Drive_data/combined.feather")
+        return combined
 
-combined = concat_csv_files()
-len(combined)
+combined_df = concat_csv_files()
+print(len(combined_df))
+
+#combined = concat_csv_files()
+#len(combined)
+
+df = pd.read_csv("data/SVRM2_Drive_data/REXCURVE 2025-09-05 084850 017.CSV",skiprows=5)
+
+print(df)
